a16f569465c0c56f1e82f7d4464b94e05bfe1842
Daniel "soro" Cunha <danielsoro@gmail.com>
2020-05-07 22:35:31 -0300

    Move load configuration from application.go to GetConfiguration func

--- application.go
+++ application.go
@@ -7,24 +7,10 @@ import (
 
 	configuration "github.com/danielsoro/go-mongo/config"
 	"github.com/danielsoro/go-mongo/database"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	var config configuration.Configuration
-
-	// Get the configuration from confi.yaml file
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("unable to load configuration file, %v", err)
-	}
-
-	if err = viper.Unmarshal(&config); err != nil {
-		log.Fatalf("unable to decode into struct, %v", err)
-	}
+	config := configuration.Configuration{}.GetConfiguation()
 
 	// Connect to the mongodb database with config properties
 	mongoDB := database.Connect(config.Database)
--- config/configuration.go
+++ config/configuration.go
@@ -1,5 +1,32 @@
 package configuration
 
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+var config Configuration
+
 type Configuration struct {
 	Database DatabaseConfiguration
+}
+
+// GetConfiguation return the application's configuration
+func (c Configuration) GetConfiguation() Configuration {
+
+	// Get the configuration from confi.yaml file
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("unable to load configuration file, %v", err)
+	}
+
+	if err = viper.Unmarshal(&config); err != nil {
+		log.Fatalf("unable to decode into struct, %v", err)
+	}
+
+	return config
 }
