8f9519593c035acb928112cb5249186f6e84b1ba
Daniel "soro" Cunha <danielsoro@gmail.com>
2020-05-22 09:11:17 -0300

    Fixing application.go

--- application.go
+++ application.go
@@ -5,18 +5,10 @@ import (
 	"fmt"
 
 	"github.com/danielsoro/go-mongo/database"
-	"github.com/danielsoro/go-mongo/models"
-	"github.com/danielsoro/go-mongo/repository"
 )
 
 func main() {
 	connect := database.Connect()
 	connect.Client().Ping(context.TODO(), nil)
 	fmt.Println("MongoDB Connected")
-
-	id := repository.TesteRepository{}.Insert(models.Teste{
-		Value: "Aloha2",
-	})
-
-	repository.TesteRepository{}.Remove(id.Hex())
 }
